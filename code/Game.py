3f52f7870c71a33c65e34bd76ae9155c941c036c
MarcosBarradas <vini.moraes.1518@gmail.com>
2025-07-07 16:23:09 -0300

    Creating obstacles with a popular technique using bidimensional array

--- code/Game.py
+++ code/Game.py
@@ -1,8 +1,9 @@
 import pygame, sys
 
 from code.Const import *
-
+from code.Obstacle import Obstacle
 from code.SpaceShip import SpaceShip
+from code.Obstacle import grid
 
 class Game:
 	def __init__(self):
@@ -10,12 +11,13 @@ class Game:
 		self.window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 		pygame.display.set_caption("VOADORES ESPACIAIS")
 		self.clock = pygame.time.Clock()
-
+		self.spaceship_group = pygame.sprite.GroupSingle()
+		self.obstacles = self.create_obstacles()
 
 	def run(self):
+		# var
 		spaceship = SpaceShip(SCREEN_WIDTH, SCREEN_HEIGHT)
-		spaceship_group = pygame.sprite.GroupSingle()
-		spaceship_group.add(spaceship)
+		self.spaceship_group.add(spaceship)
 
 		while True:
 			# Checking for events
@@ -27,12 +29,26 @@ class Game:
 
 			# To Draw
 			self.window.fill(COLOR_GREY)
-			spaceship_group.draw(self.window)
-			spaceship_group.sprite.lasers_group.draw(self.window)
+			self.spaceship_group.draw(self.window)
+			self.spaceship_group.sprite.lasers_group.draw(self.window)
+			for obstacle in self.obstacles: obstacle.blocks_group.draw(self.window)
 
 			# To update
-			spaceship_group.update()
+			self.spaceship_group.update()
 
 
 			pygame.display.update()
 			self.clock.tick(60)
+
+	@staticmethod
+	def create_obstacles():
+		obstacle_width = len(grid[0]) * 3  # Calculate the length of the obstacle
+		gap = (SCREEN_WIDTH - (4 * obstacle_width)) / 5  # Calculates the gap between obstacles
+		obstacles = []
+
+		for i in range(4):
+			off_x = (i + 1) * gap + i * obstacle_width
+			obstacle = Obstacle(off_x, SCREEN_HEIGHT - 200)
+			obstacles.append(obstacle)
+
+		return obstacles
---
+++ code/Obstacle.py
@@ -0,0 +1,37 @@
+import pygame
+
+from code.Const import COLOR_BLUE
+
+
+class Block(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		self.image = pygame.Surface((3,3))
+		self.image.fill(COLOR_BLUE)
+		self.rect = self.image.get_rect(topleft = (x, y))
+
+grid = [
+[0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]]
+
+
+	# technique frequently used by large channels such as "tech with tim" ou "clear code"
+class Obstacle:
+	def __init__(self, x, y):
+		self.blocks_group = pygame.sprite.Group()
+		for i in range(len(grid)):
+			for j in range(len(grid[i])):
+				if grid[i][j] == 1:
+					block = Block(x + (j*3), y + (i*3))  # create blocks dynamically
+					self.blocks_group.add(block)
