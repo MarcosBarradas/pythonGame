5099bf0ad49d47e2d11ea319ca42a46373f2d911
MarcosBarradas <vini.moraes.1518@gmail.com>
2025-07-07 19:29:06 -0300

    Creating aliens Implementing lasers to the aliens Implemementing movements to the left and to the right in loop

--- code/Alien.py
+++ code/Alien.py
@@ -9,3 +9,9 @@ class Alien(pygame.sprite.Sprite):
 
 	def update(self, direction):
 		self.rect.x += direction
+
+class MysteryShip(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		self.image = pygame.image.load("./assets/mystery_ship.png")
+		self.rect = self.image.get_rect(topleft = (x, 40))
